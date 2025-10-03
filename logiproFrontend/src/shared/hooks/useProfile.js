3d794533052a48075cf43fb5ba58d5723664f6e7
Jose Pereyra <malon_80@hotmail.es>
2025-10-03 16:38:01 -0300

    chore(cleanup): actualizar useProfile.js de @/components/alerts/swal a @shared y remover stub

--- logiproFrontend/src/shared/hooks/useProfile.js
+++ logiproFrontend/src/shared/hooks/useProfile.js
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUser, subscribe } from "@shared/utils/session";
-import { alertConfirm, alertError, alertSuccess } from "@/components/alerts/swal";
+import { alertConfirm, alertError, alertSuccess } from "@shared/components/alerts/swal";
 import { logout } from "@/services/auth.api";
 
 export default function useProfile() {
